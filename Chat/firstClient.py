08efb3b2ef856cdbb08fb145fe68c58a6e89b34f
PR0TEX <protex19@protonmail.com>
2023-06-15 23:26:32 +0200

    not working file exchange

--- Chat/firstClient.py
+++ Chat/firstClient.py
@@ -1,10 +1,17 @@
 import socket
+import os
 
 room_socket = None
+BUFFER_SIZE = 4096
 
-def create_room(ip, port):
+def create_room_udp(ip, port):
     global room_socket
     room_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    room_socket.bind((ip, port))
+
+def create_room_tcp(ip, port):
+    global room_socket
+    room_socket = socket.socket()
     room_socket.bind((ip, port))
 
 
@@ -13,13 +20,39 @@ def send_message(ip, port, socket, message):
     if response:
         print("message successfully send\n")
 
-def send_file(filename):
-    with open(filename, "rb") as file:
-        # Read and send the file in chunks
-        chunk = file.read(1024)
-        while chunk:
-            room_socket.send(chunk)
-            chunk = file.read(1024)
+def send_file(filename, ip, port):
+    global room_socket
+    # with open(filename, "rb") as file:
+    #     # Read and send the file in chunks
+    #     chunk = file.read(1024)
+    #     while chunk:
+    #         room_socket.send(chunk)
+    #         chunk = file.read(1024)
+    filesize = os.path.getsize(filename)
+    create_room_tcp("192.168.1.24", 2222)
+    room_socket.sendto(f"{filename}".encode(), ip, port)
+
+def receive_file():
+    create_room_tcp("192.168.1.24", 2222)
+    response = room_socket.recvfrom(BUFFER_SIZE)
+    while True:
+        if response:
+            client_socket, address = room_socket.accept()
+            filename = client_socket.recv(BUFFER_SIZE).decode()
+            filename = os.path.basename(filename)
+            break
+
+    with open(filename, "wb") as f:
+        while True:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:
+                # nothing is received
+                # file transmitting is done
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+
 
 def start_room():
     print("Room started")
@@ -46,8 +79,9 @@ def close_room():
 
 
 def main():
-    create_room("192.168.1.24", 2222)
-    start_room()
+    create_room_tcp("192.168.1.24", 2222)
+    receive_file()
+    # start_room()
     close_room()
 
 
--- Chat/secondClient.py
+++ Chat/secondClient.py
@@ -1,12 +1,35 @@
 import socket
+import os
 
 my_socket = None
-
+BUFFER_SIZE = 4096
 
 def send_message(ip, port, socket, message):
     response = socket.sendto(message.encode(), (ip, port))
     if response:
         print("message successfully send\n")
+
+def send_file(ip, port, filename):
+    # with open(filename, "rb") as file:
+    #     # Read and send the file in chunks
+    #     chunk = file.read(1024)
+    #     while chunk:
+    #         room_socket.send(chunk)
+    #         chunk = file.read(1024)
+    filesize = os.path.getsize(filename)
+    my_socket = socket.socket()
+    my_socket.connect((ip, port))
+    my_socket.sendto(f"{filename}".encode(), (ip, port))
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in
+            # busy networks
+            my_socket.sendall(bytes_read)
 
 
 def reply_message():
@@ -42,13 +65,15 @@ def close_room():
 
 def main():
     global my_socket
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     room_ip = input("Enter IP to start chat\n")
     room_port = 2222
-    init_message = input("Enter message \n")
-    print("Connection started\n")
-    send_message(room_ip, room_port, my_socket, init_message)
-    reply_message()
+    # init_message = input("Enter message \n")
+    # print("Connection started\n")
+    # send_message(room_ip, room_port, my_socket, init_message)
+    # reply_message()
+
+    send_file(room_ip, room_port, "test.txt")
 
 
 if __name__ == "__main__":
