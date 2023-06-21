1c91c5b54810e526340dbf5e95b7d24231cb0c19
Pawel Domaradzki <pawel.domaradzki@dynatrace.com>
2023-06-21 20:19:53 +0200

    rsa_generator

---
+++ AppWindow/rsa_generator.py
@@ -0,0 +1,53 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization,hashes
+
+def generate_rsa(key_size):
+
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=key_size)
+    public_key = private_key.public_key()
+
+    save_to_pem(public_key, "public_key.pem", False)
+    save_to_pem(private_key, "private_key.pem", True)
+
+    return private_key, public_key
+
+def save_to_pem(key, name, is_private):
+    if is_private:
+        with open(name, "wb") as f:
+            f.write(key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+    else:
+        with open(name, "wb") as f:
+            f.write(key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            ))
+
+def sign_message(message, private_key):
+    return private_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+def verify_sender(signature, message, public_key):
+    public_key.verify(
+        signature,
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256())
+
+
+private_key, public_key = generate_rsa(2048)
+
+print("Private Key:\n", private_key.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption()).decode())
+print("Public Key:\n", public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo).decode())
