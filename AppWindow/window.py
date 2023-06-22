b301a1c6c9a7d869e39ae0bef1328c39b9677de8
Varker00 <lukaszhein00@gmail.com>
2023-06-22 16:45:49 +0200

    changed file chunk size

--- AppWindow/window.py
+++ AppWindow/window.py
@@ -53,35 +53,35 @@ class RSAkeys:
         return message
 
 
+
 class AESCipher:
     def __init__(self, key, mode):
         self.key = md5(key.encode('utf8')).digest()
         self.mode = mode
-        self.block_size = 32
 
     def encrypt(self, data):
-        key = 'abcdefghijklmnop'
         if self.mode == "CBC":
-            iv = get_random_bytes(self.block_size)
+            iv = get_random_bytes(AES.block_size)
             self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
-            cipher_data = b64encode(iv + self.cipher.encrypt(pad(data.encode('utf-8'), self.block_size)))
+            cipher_data = b64encode(iv + self.cipher.encrypt(pad(data.encode('utf-8'), AES.block_size)))
         elif self.mode == "ECB":
-            cipher = AES.new(key.encode('utf8'), AES.MODE_ECB)
-            cipher_data = cipher.encrypt(pad(data.decode("utf-8"), self.block_size))
+            data = pad(data.encode('utf-8'), AES.block_size)
+            cipher = AES.new(self.key, AES.MODE_ECB)
+            cipher_data = base64.b64encode(cipher.encrypt(data))
         else:
             return data
 
         return cipher_data
 
     def decrypt(self, data):
-        key = 'abcdefghijklmnop'
         if self.mode == "CBC":
             raw = b64decode(data)
-            self.cipher = AES.new(self.key, AES.MODE_CBC, raw[:self.block_size])
-            decrypted_data = unpad(self.cipher.decrypt(raw[self.block_size:]), self.block_size)
+            self.cipher = AES.new(self.key, AES.MODE_CBC, raw[:AES.block_size])
+            decrypted_data = unpad(self.cipher.decrypt(raw[AES.block_size:]), AES.block_size)
         elif self.mode == "ECB":
-            decipher = AES.new(key.encode('utf8'), AES.MODE_ECB)
-            decrypted_data = decipher.decrypt(data)
+            raw = base64.b64decode(data)
+            cipher = AES.new(self.key, AES.MODE_ECB)
+            decrypted_data = unpad(cipher.decrypt(raw), AES.block_size)
         else:
             return data
         return decrypted_data
